e2a4dab95595e4ca527594af9955cb633164d615
Elena Zhao <elena@Elenas-MacBook-Air.local>
2023-04-14 16:47:20 -0700

    change http to https

--- react-frontend/src/services/UserService.js
+++ react-frontend/src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = "http://34.219.59.239:8080/api/users";
+const USER_API_BASE_URL = "https://34.219.59.239:8080/api/users";
 // const USER_API_BASE_URL = "http://localhost:8080/api/users";
 
 class UserService {

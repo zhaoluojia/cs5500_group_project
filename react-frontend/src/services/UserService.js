fdf1a199adc763bcfde23067c9e6a736b94eb0f6
cccru8 <cs.cccru8@gmail.com>
2023-04-16 12:14:26 -0700

    test http

--- react-frontend/src/services/UserService.js
+++ react-frontend/src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = "https://exercisemanager.net:8080/api/users/Alice/userId";
+const USER_API_BASE_URL = "http://exercisemanager.net:8080/api/users/Alice/userId";
 // const USER_API_BASE_URL = "http://localhost:8080/api/users";
 
 class UserService {
